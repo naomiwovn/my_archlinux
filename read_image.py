8835707b1558d2fca0112869f83db233afd662c1
naomiwovn <naomiwovn@gmail.com>
2022-01-19 23:38:58 +0000

    added cmd argument to choose image

--- read_image.py
+++ read_image.py
@@ -1,7 +1,17 @@
 import cv2
 import pytesseract
+import sys
+'''
+Prints text from image 
+'''
 
-img=cv2.imread('data/user.png')
+args = sys.argv
+
+if len(sys.argv) < 2:
+    print("Usage: python read_image.py <image>")
+
+image_path = str(args[1])
+img=cv2.imread(image_path)
 
 text=pytesseract.image_to_string(img)
 print(text)
